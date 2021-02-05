0bda295e087da7d75db1970b52279a324d3d3a88
douglasphellipe <douglasphellipe2003@gmail.com>
2021-02-05 13:54:24 -0300

    Update db_imobiliaria.sql

--- db_imobiliaria.sql
+++ db_imobiliaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 24-Nov-2019 às 02:10
+-- Generation Time: 05-Fev-2021 às 16:52
 -- Versão do servidor: 5.7.26
 -- versão do PHP: 7.2.18
 
@@ -21,8 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_imobiliaria`
 --
-CREATE DATABASE IF NOT EXISTS `db_imobiliaria` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db_imobiliaria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_adm`
+--
+
+DROP TABLE IF EXISTS `tb_adm`;
+CREATE TABLE IF NOT EXISTS `tb_adm` (
+  `id_adm` varchar(60) NOT NULL,
+  `senha_adm` varchar(80) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_adm`
+--
+
+INSERT INTO `tb_adm` (`id_adm`, `senha_adm`) VALUES
+('', ''),
+('', ''),
+('', ''),
+('', '1234657'),
+('', '1234765'),
+('7894', 'goga08');
 
 -- --------------------------------------------------------
 
@@ -34,14 +56,18 @@ DROP TABLE IF EXISTS `tb_forms`;
 CREATE TABLE IF NOT EXISTS `tb_forms` (
   `Nome` varchar(60) NOT NULL,
   `Email` varchar(80) NOT NULL,
-  `Mensagem` varchar(100) NOT NULL,
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `Mensagem` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `tb_forms`
+--
 
-DROP TABLE IF EXISTS `tb_adm`;
-CREATE TABLE IF NOT EXISTS `tb_adm` (
-  `id_adm` varchar(60) NOT NULL,
-  `senha_adm` varchar(80) NOT NULL,
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+INSERT INTO `tb_forms` (`Nome`, `Email`, `Mensagem`) VALUES
+('Douglas Phellipe Martins Silva', 'd', 'aaaaa'),
+('Douglas Phellipe Martins Silva', 'ssa', 'aaaaa');
+COMMIT;
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
